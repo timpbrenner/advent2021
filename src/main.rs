1fe04043379fbf8891c6740c51e5b7ab5b0ad212
Tim Brenner <timpbrenner@gmail.com>
2021-12-06 20:24:40 -0500

    Bingo Day 4

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     move_sub();
     get_power();
     get_o2_and_co2();
+    bingo();
+    last_bingo();
 }
 
 fn do_stuff() {
@@ -216,4 +218,176 @@ fn get_co2(lines: Vec<String>, char_pos: i32) -> Vec<String> {
     }
 
     return lines.into_iter().filter(|line| line.chars().nth(char_pos.try_into().unwrap()).unwrap() ==  least_popular ).collect();
+}
+
+#[derive(Debug, Clone)]
+struct BingoBoard {
+    board: Vec::<Vec::<i32>>,
+    selection: Vec::<Vec::<i32>>,
+}
+
+impl BingoBoard {
+    fn new() -> BingoBoard {
+        return BingoBoard {
+            board: vec![vec![0; 5]; 5],
+            selection: vec![vec![0; 5]; 5],
+        };
+    }
+
+    fn make_selection(&mut self, selection: i32) {
+        for x in 0..5 {
+            for y in 0..5 {
+                if self.board[y][x] == selection {
+                    self.selection[y][x] = 1;
+                }
+            }
+        }
+    }
+
+    fn winner(&self) -> bool {
+        for x in 0..5 {
+            if self.selection[x] == vec![1,1,1,1,1] {
+                return true;
+            }
+        }
+
+        for y in 0..5 {
+            let column = vec![
+                self.selection[0][y],
+                self.selection[1][y],
+                self.selection[2][y],
+                self.selection[3][y],
+                self.selection[4][y],
+            ];
+
+            if column == vec![1,1,1,1,1] {
+                   return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn score(&self) -> i32 {
+        let mut sum = 0;
+
+        for x in 0..5 {
+            for y in 0..5 {
+                if self.selection[y][x] == 0 {
+                    sum += self.board[y][x];
+                }
+            }
+        }
+
+        return sum;
+    }
+}
+
+fn bingo() {
+    let mut file = File::open("data/day4.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let mut lines = contents.lines();
+    let selections = lines.next().unwrap().to_string();
+
+    lines.next();
+
+    let boards = populate_boards(lines);
+
+    let winner = make_selections(boards, selections).unwrap();
+    let board = winner.board;
+    println!("Board Score: {} Last: {} Score: {}", board.score(), winner.last_number, winner.last_number * board.score());
+}
+
+fn last_bingo() {
+    let mut file = File::open("data/day4.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let mut lines = contents.lines();
+    let selections = lines.next().unwrap().to_string();
+
+    lines.next();
+
+    let boards = populate_boards(lines);
+
+    let winner = make_last_selections(boards, selections).unwrap();
+    let board = winner.board;
+    println!("Last Board Score: {} Last: {} Score: {}", board.score(), winner.last_number, winner.last_number * board.score());
+}
+
+use std::str::Lines;
+
+fn populate_boards(lines: Lines) -> Vec<BingoBoard> {
+    let mut boards = Vec::<BingoBoard>::new();
+
+    let mut line_count = 0;
+    let mut board = BingoBoard::new();
+
+    for line in lines {
+        if line_count == 5 {
+            line_count = 0;
+            continue;
+        }
+
+        board.board[line_count] = line.split_whitespace().map(|s| s.parse::<i32>().unwrap() ).collect();
+
+        if line_count == 4 {
+            boards.push(board);
+            board = BingoBoard::new();
+        }
+
+        line_count += 1;
+    }
+
+    return boards;
+}
+
+struct Winner {
+    board: BingoBoard,
+    last_number: i32,
+}
+
+fn make_selections(mut boards: Vec<BingoBoard>, selections: String) -> Option<Winner> {
+    for selection in selections.split(',') {
+        for board in boards.iter_mut() {
+            let pull = selection.parse::<i32>().unwrap();
+            board.make_selection(pull);
+
+            if board.winner() {
+                let winner = Winner {
+                    board: board.clone(),
+                    last_number: pull,
+                };
+
+                return Some(winner);
+            }
+        }
+    }
+
+    None
+}
+
+fn make_last_selections(mut boards: Vec<BingoBoard>, selections: String) -> Option<Winner> {
+    for selection in selections.split(',') {
+        let pull = selection.parse::<i32>().unwrap();
+
+        for board in boards.iter_mut() {
+            board.make_selection(pull);
+        }
+
+        if boards.len() > 1 {
+            boards.retain(|b| !b.winner());
+        } else if boards[0].winner() {
+            let winner = Winner {
+                board: boards[0].clone(),
+                last_number: pull,
+            };
+
+            return Some(winner);
+        }
+    }
+
+    None
 }
