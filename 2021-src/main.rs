e4d3ab81c1c1fd4367ad4471e1a5c326c5fb9192
Tim Brenner <timpbrenner@gmail.com>
2022-12-03 14:00:42 -0500

    Move 2021 and add 2022 day 1

---
+++ 2021-src/main.rs
@@ -0,0 +1,31 @@
+mod days;
+
+use crate::days::day1::{compare_ranges, get_depth};
+use crate::days::day2::move_sub;
+use crate::days::day3::{get_power, get_o2_and_co2};
+use crate::days::day4::{bingo, last_bingo};
+use crate::days::day5::grid;
+use crate::days::day6::lanternfish_count;
+use crate::days::day7::crab_sub;
+use crate::days::day8::weird_number_thing;
+use crate::days::day9::low_danger;
+use crate::days::day10::syntax_matching;
+use crate::days::day11::flashes;
+
+fn main() {
+    get_depth();
+    compare_ranges(3);
+    move_sub();
+    get_power();
+    get_o2_and_co2();
+    bingo();
+    last_bingo();
+    grid();
+    lanternfish_count();
+    crab_sub();
+    weird_number_thing();
+    low_danger();
+    syntax_matching();
+
+    flashes();
+}
--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,74 +1,24 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-pub fn get_depth() {
-    let mut file = File::open("data/input.txt").unwrap();
+pub fn get_calories() {
+    let mut file = File::open("data/day1.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
-    let mut lines = contents.lines();
-    let mut previous = lines.next().unwrap().parse::<i32>().unwrap();
-    let mut depth_count = 0;
+    let mut current_calories: i32 = 0;
+    let mut calories: Vec<i32> = Vec::new();
 
-    for line in lines {
-        let current = line.parse::<i32>().unwrap();
-
-        if current > previous {
-            depth_count += 1;
-        }
-
-        previous = current;
+    for line in contents.lines() {
+      if line == "" {
+        calories.push(current_calories);
+        current_calories = 0;
+      } else {
+        let cal = line.parse::<i32>().unwrap();
+        current_calories += cal;
+      }
     }
-
-    println!("Depth Count: {}", depth_count);
-}
-
-pub fn compare_ranges(_array_count: i32) {
-    let mut ranges: Vec<Vec<i32>> = Vec::from([
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-    ]);
-
-    let mut file = File::open("data/input.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-    let mut lines = contents.lines();
-
-    let first = lines.next().unwrap().parse::<i32>().unwrap();
-    ranges[0].push(first);
-
-    let second = lines.next().unwrap().parse::<i32>().unwrap();
-    ranges[0].push(second);
-    ranges[1].push(second);
-
-    let third = lines.next().unwrap().parse::<i32>().unwrap();
-    ranges[0].push(third);
-    ranges[1].push(third);
-    ranges[2].push(third);
-
-    let mut current_index = 0;
-    let mut depth_count = 0;
-    for line in lines {
-        let current = line.parse::<i32>().unwrap();
-
-        ranges[(current_index + 1) % 4].push(current);
-        ranges[(current_index + 2) % 4].push(current);
-        ranges[(current_index + 3) % 4].push(current);
-
-        let range_sum: i32 = ranges[current_index].iter().sum();
-        let second_sum: i32 = ranges[(current_index + 1) % 4].iter().sum();
-
-        if second_sum > range_sum {
-            depth_count += 1;
-        }
-
-        ranges[current_index] = Vec::new();
-
-        current_index = (current_index + 1) % 4;
-    }
-
-    println!("Range Depth Count: {}", depth_count);
+    calories.sort_by(|a, b| b.cmp(a));
+    println!("Most Calories: {}", calories[0]);
+    println!("Top 3: {}", calories[0] + calories[1] + calories[2]);
 }
