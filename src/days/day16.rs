441720eded94cb373ee1478205403e1d33cc0432
Tim Brenner <timpbrenner@gmail.com>
2022-12-19 13:26:37 -0500

    Day 16 sucks

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -5,15 +5,16 @@ use std::collections::HashMap;
 use std::io;
 use std::collections::VecDeque;
 
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug)]
 struct Valve {
   name: String,
   flow: u32,
   branches: Vec<String>,
+  paths: HashMap<String, VecDeque<String>>,
 }
 
 pub fn pipes() {
-  let valves = get_valves();
+  let mut valves = get_valves();
 
   let mut flow:u32 = 0;
   let mut opened: Vec<String> = Vec::new();
@@ -24,7 +25,9 @@ pub fn pipes() {
     println!("_____________________");
 
     flow += get_flow(&valves, &opened);
-    tick(&mut current, &valves, &mut opened);
+    tick(&mut current, &mut valves, &mut opened, min);
+
+   break;
   }
 
    println!("FLOW: {}", flow);
@@ -34,7 +37,7 @@ fn get_flow(valves: &HashMap<String, Valve>, opened: &Vec<String>) -> u32 {
   opened.iter().map(|v| valves[v].flow ).sum()
 }
 
-fn tick(current:&mut String, valves: &HashMap<String, Valve>, opened: &mut Vec<String>) {
+fn tick(current:&mut String, valves: &mut HashMap<String, Valve>, opened: &mut Vec<String>, min:u32) {
   let current_valve = valves[current].clone();
   if !opened.contains(&current) && current_valve.flow > 0{
     opened.push(current.clone());
@@ -42,7 +45,8 @@ fn tick(current:&mut String, valves: &HashMap<String, Valve>, opened: &mut Vec<S
     return;
   }
 
-  let t = get_target(valves, opened);
+  populate_paths(current, valves);
+  let t = get_target(current, valves, opened, min);
   if t.is_none() {
     return;
   }
@@ -97,13 +101,39 @@ fn rec_path(valves: &HashMap<String, Valve>, current: &String, target: String, p
   return r_path;
 }
 
-fn get_target(valves: &HashMap<String, Valve>, opened: &mut Vec<String>) -> Option<String> {
-  let result = valves.values().filter(|v| v.flow > 0 && !opened.contains(&v.name)).max_by_key(|x| x.flow);
+fn get_target(current: &String, valves: &HashMap<String, Valve>, opened: &mut Vec<String>, min:u32) -> Option<String> {
+  let current = valves[current].clone();
+  println!("PATHS: {:?}", current.paths.values());
+  for (name, path) in current.paths {
+    let target = valves[&name].clone();
+    println!("PATH: {} - {:?}", name, path);
+    let time_left = 30 - min - path.len() as u32;
+    let expected_amount = time_left * target.flow;
+
+    println!("{} * {} = {}", time_left, target.flow, expected_amount);
+  }
+
+  let result = valves.values().filter(|v| v.flow > 0 && !opened.contains(&v.name)).max_by_key(|v|{
+    v.flow
+  });
+
   if result.is_none() {
     return None;
   }
 
   Some(result.unwrap().name.clone())
+}
+
+fn populate_paths(current:&String, valves: &mut HashMap<String, Valve>) {
+  let pass_in = valves.clone();
+  let current_valve = valves.get_mut(current).unwrap();
+  for (name, valve) in &pass_in {
+    if valve.flow == 0 {
+      continue;
+    }
+
+    current_valve.paths.insert(name.clone(), get_path(&pass_in, (*current).clone(), name.clone()));
+  }
 }
 
 fn get_valves() -> HashMap<String, Valve> {
@@ -118,7 +148,8 @@ fn get_valves() -> HashMap<String, Valve> {
     valves.insert(name.clone(), Valve {
       name: name.clone(),
       flow: cap[2].parse::<u32>().unwrap(),
-      branches: cap[3].to_string().split(", ").map(|x| x.to_string()).collect::<Vec<String>>()
+      branches: cap[3].to_string().split(", ").map(|x| x.to_string()).collect::<Vec<String>>(),
+      paths: HashMap::new()
     });
   }
 
